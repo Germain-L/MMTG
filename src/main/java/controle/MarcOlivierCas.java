ad1ea4e109e303bfcfe50a083ca268f30b309920
Marc-Olivier-CAS <marcolivier_cas@yahoo.fr>
2022-11-10 11:31:51 +0100

    "commit after dev pull"

--- src/main/java/controle/MarcOlivierCas.java
+++ src/main/java/controle/MarcOlivierCas.java
@@ -1,2 +1,7 @@
-package controle;public class MarcOlivierCas {
+package controle;
+
+public class MarcOlivierCas {
+    public void MarcOlivierCas() {
+        System.out.println("Hello, this is a class created and modified by Marc-Olivier CAS");
+    }
 }
